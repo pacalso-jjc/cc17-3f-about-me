7191d477b689b77118d236a251a6a5d81e6c7a18
JerryJuniorPacalso <pacalsojerry@gmail.com>
2024-09-22 23:06:16 +0800

    Add recycler view for list of about mes

--- app/src/main/java/com/example/cc17_3f_aboutme/MainActivity.kt
+++ app/src/main/java/com/example/cc17_3f_aboutme/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cc17_3f_aboutme.adapter.AboutMeAdapter
+import com.example.cc17_3f_aboutme.model.AboutMe
+import java.util.Arrays
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val rvAboutMe: RecyclerView = findViewById(R.id.rvAboutMe)
+        rvAboutMe.layoutManager = LinearLayoutManager(this)
+
+        val aboutMe1 = AboutMe(R.mipmap.ic_launcher,
+            "Jerry Junior Pacalso",
+            "Short Desc",
+            { /*TODO*/} )
+
+        val aboutMe2 = AboutMe(R.mipmap.ic_launcher,
+            "Jane Doe",
+            "ZZZ",
+            {/* TODO */})
+
+        val arrList = listOf(aboutMe1, aboutMe2)
+
+        rvAboutMe.adapter = AboutMeAdapter(arrList)
     }
 }
