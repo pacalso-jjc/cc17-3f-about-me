af7d8b922c500c173fcc99c16c7ddedd9923478c
TrinaIlyza <tds4428@students.uc-bcf.edu.ph>
2024-09-27 21:38:48 +0800

    add trina ilyza show more page

--- app/src/main/java/com/example/cc17_3f_aboutme/MainActivity.kt
+++ app/src/main/java/com/example/cc17_3f_aboutme/MainActivity.kt
@@ -127,9 +127,10 @@ class MainActivity : AppCompatActivity() {
         val sobrepenatid = AboutMe(R.drawable.sobrepenatid,
             "Trina Ilyza Sobrepeña",
             "Hot Dancer",
-            { /*view ->
-            val intent = Intent(this, AboutTrinaActivity::class.java)
-            startActivity((intent)*/})
+            { view ->
+            val intent = Intent(this, AboutTrinaIlyza ::class.java)
+            startActivity(intent)
+            })
 
 
         val arrList = listOf(aboutMe1, aboutMe2, johnSmith, isaac, jLumague, siachongco, jBravo, secolles, PastorKJ, ramos, payumo, garduque, ailaNieva, sobrepenatid)
