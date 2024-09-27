d5e50b7c051f573d401c14aa34f215ff1e567332
Simon Aquino <simonadriandeguzmanaquino@gmail.com>
2024-09-28 00:16:10 +0800

    push push pushhh

--- app/src/main/java/com/example/cc17_3f_aboutme/AboutSimonAquinoActivity.kt
+++ app/src/main/java/com/example/cc17_3f_aboutme/AboutSimonAquinoActivity.kt
@@ -1,11 +1,10 @@
-package com.example.cc17_3f_aboutme.model
+package com.example.cc17_3f_aboutme
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.cc17_3f_aboutme.R
 
 class AboutSimonAquinoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/cc17_3f_aboutme/MainActivity.kt
+++ app/src/main/java/com/example/cc17_3f_aboutme/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cc17_3f_aboutme.adapter.AboutMeAdapter
 import com.example.cc17_3f_aboutme.model.AboutMe
-import com.example.cc17_3f_aboutme.model.AboutSimonAquinoActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
