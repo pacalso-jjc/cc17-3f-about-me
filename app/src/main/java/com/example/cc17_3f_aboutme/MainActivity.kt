f940b0b5adbaef82ed308a4f6b7f1a46e17105df
Simon Aquino <simonadriandeguzmanaquino@gmail.com>
2024-09-27 22:05:35 +0800

    Add Simon Adrian Aquino to the list of about me

--- app/src/main/java/com/example/cc17_3f_aboutme/MainActivity.kt
+++ app/src/main/java/com/example/cc17_3f_aboutme/MainActivity.kt
@@ -134,8 +134,15 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AboutTrinaActivity::class.java)
             startActivity((intent)*/})
 
+        val simonKao = AboutMe(R.drawable.kaokao,
+            "Simon Adrian Aquino",
+            "Pight Me",
+            { /*TODO*/
+            },
+        )
 
-        val arrList = listOf(aboutMe1, aboutMe2, johnSmith, isaac, jLumague, siachongco, jBravo, secolles, PastorKJ, ramos, payumo, garduque, ailaNieva, sobrepenatid)
+
+        val arrList = listOf(aboutMe1, aboutMe2, johnSmith, isaac, jLumague, siachongco, jBravo, secolles, PastorKJ, ramos, payumo, garduque, ailaNieva, sobrepenatid, simonKao)
 
 
         rvAboutMe.adapter = AboutMeAdapter(arrList)
